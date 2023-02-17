97f22d83a3d75f4723b2adbe1621ba1f7ac87eee
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-02-18 02:59:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,377 @@
+let data = [
+    {
+        "name": "Laurence Hoeger",
+        "email": "Rory83@gmail.com",
+        "id": "1",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Lucas Walsh",
+        "email": "Jacky.Rempel@gmail.com",
+        "id": "2",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Eva Leannon",
+        "email": "Tyshawn.Koch@gmail.com",
+        "id": "3",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Todd Howell",
+        "email": "Lester81@hotmail.com",
+        "id": "4",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Mack Gutmann",
+        "email": "Chelsie56@gmail.com",
+        "id": "5",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Drew Hammes",
+        "email": "Claude34@yahoo.com",
+        "id": "6",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Tyler Johnston",
+        "email": "Greg_Krajcik90@gmail.com",
+        "id": "7",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Fannie Renner",
+        "email": "Jarred3@hotmail.com",
+        "id": "8",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Brendan Abernathy",
+        "email": "Toney.Rodriguez15@hotmail.com",
+        "id": "9",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Bernadette Swaniawski",
+        "email": "Peggie.Considine@hotmail.com",
+        "id": "10",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Emmett Borer",
+        "email": "Rosa25@yahoo.com",
+        "id": "11",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Ms. Donna Doyle",
+        "email": "Ayana.Stroman80@hotmail.com",
+        "id": "12",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Hilda Kunze",
+        "email": "Cecilia58@gmail.com",
+        "id": "13",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Sonya O'Kon",
+        "email": "Marguerite_Jakubowski29@hotmail.com",
+        "id": "14",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Diane Klein",
+        "email": "Mervin.Quitzon38@gmail.com",
+        "id": "15",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Marcella Marks",
+        "email": "Velda60@yahoo.com",
+        "id": "16",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Meghan Rau",
+        "email": "Troy77@gmail.com",
+        "id": "17",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Kellie Aufderhar III",
+        "email": "Brice.Beahan@yahoo.com",
+        "id": "18",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Mildred Vandervort",
+        "email": "Buddy19@yahoo.com",
+        "id": "19",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Dr. Bridget Turcotte",
+        "email": "Peggie_Koss@gmail.com",
+        "id": "20",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Ronnie Kuhic Sr.",
+        "email": "Stacy70@yahoo.com",
+        "id": "21",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Rudolph Roberts",
+        "email": "Dustin3@gmail.com",
+        "id": "22",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Barry Hayes",
+        "email": "Stanford54@gmail.com",
+        "id": "23",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Lance Kuvalis",
+        "email": "Earline_Parisian@gmail.com",
+        "id": "24",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Sandra Cormier",
+        "email": "Gia.Satterfield@yahoo.com",
+        "id": "25",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Judy Price",
+        "email": "Deondre.King@gmail.com",
+        "id": "26",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Leah Nienow",
+        "email": "Gladys20@gmail.com",
+        "id": "27",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Arlene Considine",
+        "email": "Willy_Effertz81@hotmail.com",
+        "id": "28",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Jeanne Block III",
+        "email": "Will.Green39@hotmail.com",
+        "id": "29",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Mr. Patricia Lemke",
+        "email": "Greg44@yahoo.com",
+        "id": "30",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Lucille Batz",
+        "email": "Mose70@hotmail.com",
+        "id": "31",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Donna Krajcik",
+        "email": "Ona_Grady35@hotmail.com",
+        "id": "32",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Arturo Dicki",
+        "email": "Carey.Labadie@hotmail.com",
+        "id": "33",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Louis Predovic",
+        "email": "Lucius.Bailey29@hotmail.com",
+        "id": "34",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Dana Shanahan",
+        "email": "Susanna.Gerhold75@gmail.com",
+        "id": "35",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "John Ritchie",
+        "email": "Marianna.Abbott@gmail.com",
+        "id": "36",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Miss Victoria Homenick",
+        "email": "Eric_Stoltenberg@hotmail.com",
+        "id": "37",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Sean Turcotte",
+        "email": "Gisselle.Jaskolski52@gmail.com",
+        "id": "38",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Jan O'Reilly",
+        "email": "Clare_Goodwin@yahoo.com",
+        "id": "39",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Ms. Dean Blick",
+        "email": "Robert.Bergnaum@gmail.com",
+        "id": "40",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Merle Stehr",
+        "email": "Modesto.Kuhn24@yahoo.com",
+        "id": "41",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Tom Rodriguez",
+        "email": "Breana_Weissnat@hotmail.com",
+        "id": "42",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Antoinette Stiedemann",
+        "email": "Frederic64@gmail.com",
+        "id": "43",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Guadalupe Reilly",
+        "email": "Scot.Kuhlman@gmail.com",
+        "id": "44",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Juana Swift",
+        "email": "Elna_Hegmann@hotmail.com",
+        "id": "45",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Randall Zboncak MD",
+        "email": "Brad52@hotmail.com",
+        "id": "46",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Angel Barton I",
+        "email": "Easter.Herman87@hotmail.com",
+        "id": "47",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Gayle Walker",
+        "email": "Susie.Osinski@yahoo.com",
+        "id": "48",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Debra Crooks",
+        "email": "Bernita_Harvey78@yahoo.com",
+        "id": "49",
+        "mobile":"1234567899"
+    },
+    {
+        "name": "Nora Lynch",
+        "email": "Betty30@yahoo.com",
+        "id": "50",
+        "mobile":"1234567899"
+    }
+];
+
+var docu = {
+    querySet: data,
+    page: 1,
+    rows: 5,
+    window: 5
+};
+
+buildTable();
+
+function pagination(querySet, page, rows) {
+    var trimStart = (page - 1) * rows;
+    var trimEnd = trimStart + rows;
+    var trimmedData = querySet.slice(trimStart, trimEnd);
+    var pages = Math.round(querySet.length / rows);
+    return {
+        querySet: trimmedData,
+        pages: pages
+    };
+}
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('buttons');
+    wrapper.innerHTML = ``;
+    var maxLeft = docu.page - Math.floor(docu.window / 2);
+    var maxRight = docu.page + Math.floor(docu.window / 2);
+    if (maxLeft < 1) {
+        maxLeft = 1;
+        maxRight = docu.window;
+    }
+    if (maxRight > pages) {
+        maxLeft = pages - (docu.window - 1);
+        if (maxLeft < 1) {
+            maxLeft = 1;
+        }
+        maxRight = pages;
+    }
+    for (var page = maxLeft; page <= maxRight; page++) {
+        wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-secondary mx-2 px-3">${page}</button>`;
+    }
+    wrapper.innerHTML =
+        `<button value=${-1} class="move btn btn-sm btn-primary px-3 mx-2">&#171; Previous</button>` +
+        wrapper.innerHTML;
+    wrapper.innerHTML += `<button value=${1} class="move btn btn-sm btn-primary px-3 mx-2">next &#187;</button>`;
+
+    $('.page').on('click', function () {
+        $('#table_body').empty();
+        docu.page = Number($(this).val());
+        buildTable();
+    });
+
+    $('.move').on('click', function () {
+        $('#table_body').empty();
+        docu.page += Number($(this).val());
+        if (docu.page > 20) docu.page = 1;
+        else if (docu.page < 1) docu.page = 20;
+        buildTable();
+    });
+}
+
+function buildTable() {
+    let table = $('#table_body');
+    let data = pagination(docu.querySet, docu.page, docu.rows);
+    let myList = data.querySet;
+    for (let i in myList) {
+        let row = `<tr>
+                    <td>${myList[i].id}</td>
+                    <td>${myList[i].name}</td>
+                    <td>${myList[i].email}</td>
+                    <td>${myList[i].mobile}</td>
+                    `;
+        table.append(row);
+    }
+    pageButtons(data.pages);
+}
